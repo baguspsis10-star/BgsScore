9505b3b0c428b3af8257196331b7d51d4cf9cca4
baguspsis10-star <baguspsis10@gmail.com>
2026-08-24 22:08:31 +0700

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -1,23 +1,8 @@
 // DATA, CONSTANTS, & GLOBAL STATE MODULE
 
-// Daftar Liga Populer (Diurutkan & Diprioritaskan untuk UI)
-const POPULAR_LEAGUES = [
-  { id: 'uefa.champions', name: "UEFA Champions League", shortName: "UCL", isPopular: true },
-  { id: 'uefa.europa', name: "UEFA Europa League", shortName: "UEL", isPopular: true },
-  { id: 'uefa.europa.conf', name: "UEFA Conference League", shortName: "UECL", isPopular: true },
-  { id: 'eng.1', name: "English Premier League", shortName: "EPL", isPopular: true },
-  { id: 'esp.1', name: "Spain La Liga", shortName: "La Liga", isPopular: true },
-  { id: 'ger.1', name: "Bundesliga", shortName: "Bundesliga", isPopular: true },
-  { id: 'ita.1', name: "Italian Serie A", shortName: "Serie A", isPopular: true },
-  { id: 'fra.1', name: "French Ligue 1", shortName: "Ligue 1", isPopular: true },
-  { id: 'idn.1', name: "Liga 1 Indonesia", shortName: "Liga 1", isPopular: true },
-  { id: 'fifa.world', name: "FIFA World Cup", shortName: "World Cup", isPopular: true },
-  { id: 'afc.asian.cup', name: "AFC Asian Cup", shortName: "Asian Cup", isPopular: true }
-];
-
 const LEAGUES = [
   // International / FIFA
-  { id: 'fifa.world', name: "FIFA World Cup", country: "Internasional", flag: "🌐", logo: "https://www.thesportsdb.com/images/media/league/badge/5d4x5v1534346808.png", category: "Internasional", isPopular: true },
+  { id: 'fifa.world', name: "FIFA World Cup", country: "Internasional", flag: "🌐", logo: "https://www.thesportsdb.com/images/media/league/badge/5d4x5v1534346808.png", category: "Internasional" },
   { id: 'fifa.wwc', name: "FIFA Women's World Cup", country: "Internasional", flag: "🌐", logo: "https://www.thesportsdb.com/images/media/league/badge/5d4x5v1534346808.png", category: "Internasional" },
   { id: 'fifa.world.u20', name: "FIFA Under-20 World Cup", country: "Internasional", flag: "🌐", logo: "https://www.thesportsdb.com/images/media/league/badge/5d4x5v1534346808.png", category: "Internasional" },
   { id: 'fifa.world.u17', name: "FIFA Under-17 World Cup", country: "Internasional", flag: "🌐", logo: "https://www.thesportsdb.com/images/media/league/badge/5d4x5v1534346808.png", category: "Internasional" },
@@ -43,11 +28,11 @@ const LEAGUES = [
   { id: 'fifa.wworldq.uefa', name: "FIFA Women's World Cup Qualifying - UEFA", country: "Eropa", flag: "🇪🇺", logo: "https://www.thesportsdb.com/images/media/league/badge/f1k1a51534346903.png", category: "Internasional" },
 
   // UEFA
-  { id: 'uefa.champions', name: "UEFA Champions League", country: "Eropa", flag: "🇪🇺", logo: "https://www.thesportsdb.com/images/media/league/badge/83ip001680100782.png", category: "Piala/kompetisi", isPopular: true },
+  { id: 'uefa.champions', name: "UEFA Champions League", country: "Eropa", flag: "🇪🇺", logo: "https://www.thesportsdb.com/images/media/league/badge/83ip001680100782.png", category: "Piala/kompetisi" },
   { id: 'uefa.champions_qual', name: "UEFA Champions League Qualifying", country: "Eropa", flag: "🇪🇺", logo: "https://www.thesportsdb.com/images/media/league/badge/83ip001680100782.png", category: "Piala/kompetisi" },
-  { id: 'uefa.europa', name: "UEFA Europa League", country: "Eropa", flag: "🇪🇺", logo: "https://www.thesportsdb.com/images/media/league/badge/2d3n2k1680100806.png", category: "Piala/kompetisi", isPopular: true },
+  { id: 'uefa.europa', name: "UEFA Europa League", country: "Eropa", flag: "🇪🇺", logo: "https://www.thesportsdb.com/images/media/league/badge/2d3n2k1680100806.png", category: "Piala/kompetisi" },
   { id: 'uefa.europa_qual', name: "UEFA Europa League Qualifying", country: "Eropa", flag: "🇪🇺", logo: "https://www.thesportsdb.com/images/media/league/badge/2d3n2k1680100806.png", category: "Piala/kompetisi" },
-  { id: 'uefa.europa.conf', name: "UEFA Conference League", country: "Eropa", flag: "🇪🇺", logo: "https://www.thesportsdb.com/images/media/league/badge/4v7s941680100827.png", category: "Piala/kompetisi", isPopular: true },
+  { id: 'uefa.europa.conf', name: "UEFA Conference League", country: "Eropa", flag: "🇪🇺", logo: "https://www.thesportsdb.com/images/media/league/badge/4v7s941680100827.png", category: "Piala/kompetisi" },
   { id: 'uefa.europa.conf_qual', name: "UEFA Conference League Qualifying", country: "Eropa", flag: "🇪🇺", logo: "https://www.thesportsdb.com/images/media/league/badge/4v7s941680100827.png", category: "Piala/kompetisi" },
   { id: 'uefa.super_cup', name: "UEFA Super Cup", country: "Eropa", flag: "🇪🇺", logo: "https://www.thesportsdb.com/images/media/league/badge/f1k1a51534346903.png", category: "Piala/kompetisi" },
   { id: 'uefa.wchampions', name: "UEFA Women's Champions League", country: "Eropa", flag: "🇪🇺", logo: "https://www.thesportsdb.com/images/media/league/badge/f1k1a51534346903.png", category: "Piala/kompetisi" },
@@ -61,7 +46,7 @@ const LEAGUES = [
   { id: 'uefa.w.nations', name: "UEFA Women's Nations League", country: "Eropa", flag: "🇪🇺", logo: "https://www.thesportsdb.com/images/media/league/badge/7l0pvn1538304918.png", category: "Internasional" },
 
   // England
-  { id: 'eng.1', name: "English Premier League", country: "Inggris", flag: "🏴󠁧󠁢󠁥󠁮󠁧󠁿", logo: "https://www.thesportsdb.com/images/media/league/badge/3v933i1680099716.png", category: "Eropa", isPopular: true },
+  { id: 'eng.1', name: "English Premier League", country: "Inggris", flag: "🏴󠁧󠁢󠁥󠁮󠁧󠁿", logo: "https://www.thesportsdb.com/images/media/league/badge/3v933i1680099716.png", category: "Eropa" },
   { id: 'eng.2', name: "English Championship", country: "Inggris", flag: "🏴󠁧󠁢󠁥󠁮󠁧󠁿", logo: "https://www.thesportsdb.com/images/media/league/badge/9f3upr1680099738.png", category: "Eropa" },
   { id: 'eng.3', name: "English League One", country: "Inggris", flag: "🏴󠁧󠁢󠁥󠁮󠁧󠁿", logo: "https://www.thesportsdb.com/images/media/league/badge/3t87761680099757.png", category: "Eropa" },
   { id: 'eng.4', name: "English League Two", country: "Inggris", flag: "🏴󠁧󠁢󠁥󠁮󠁧󠁿", logo: "https://www.thesportsdb.com/images/media/league/badge/e0l07b1680099778.png", category: "Eropa" },
@@ -76,7 +61,7 @@ const LEAGUES = [
   { id: 'eng.w.charity', name: "English Women's FA Community Shield", country: "Inggris", flag: "🏴󠁧󠁢󠁥󠁮󠁧󠁿", logo: "https://www.thesportsdb.com/images/media/league/badge/f1k1a51534346903.png", category: "Piala/kompetisi" },
 
   // Spain
-  { id: 'esp.1', name: "Spain La Liga", country: "Spanyol", flag: "🇪🇸", logo: "https://www.thesportsdb.com/images/media/league/badge/k83j411680100122.png", category: "Eropa", isPopular: true },
+  { id: 'esp.1', name: "Spanish LALIGA", country: "Spanyol", flag: "🇪🇸", logo: "https://www.thesportsdb.com/images/media/league/badge/k83j411680100122.png", category: "Eropa" },
   { id: 'esp.2', name: "Spanish LALIGA 2", country: "Spanyol", flag: "🇪🇸", logo: "https://www.thesportsdb.com/images/media/league/badge/k83j411680100122.png", category: "Eropa" },
   { id: 'esp.copa_del_rey', name: "Spanish Copa del Rey", country: "Spanyol", flag: "🇪🇸", logo: "https://www.thesportsdb.com/images/media/league/badge/2522c01534347814.png", category: "Piala/kompetisi" },
   { id: 'esp.super_cup', name: "Spanish Supercopa", country: "Spanyol", flag: "🇪🇸", logo: "https://www.thesportsdb.com/images/media/league/badge/k83j411680100122.png", category: "Piala/kompetisi" },
@@ -85,7 +70,7 @@ const LEAGUES = [
   { id: 'esp.copa_de_la_reina', name: "Spanish Copa de la Reina", country: "Spanyol", flag: "🇪🇸", logo: "https://www.thesportsdb.com/images/media/league/badge/2522c01534347814.png", category: "Piala/kompetisi" },
 
   // Germany
-  { id: 'ger.1', name: "Bundesliga", country: "Jerman", flag: "🇩🇪", logo: "https://www.thesportsdb.com/images/media/league/badge/0319201680099902.png", category: "Eropa", isPopular: true },
+  { id: 'ger.1', name: "German Bundesliga", country: "Jerman", flag: "🇩🇪", logo: "https://www.thesportsdb.com/images/media/league/badge/0319201680099902.png", category: "Eropa" },
   { id: 'ger.2', name: "German 2. Bundesliga", country: "Jerman", flag: "🇩🇪", logo: "https://www.thesportsdb.com/images/media/league/badge/0319201680099902.png", category: "Eropa" },
   { id: 'ger.dfb_pokal', name: "German Cup", country: "Jerman", flag: "🇩🇪", logo: "https://www.thesportsdb.com/images/media/league/badge/2522c01534347814.png", category: "Piala/kompetisi" },
   { id: 'ger.super_cup', name: "German Supercup", country: "Jerman", flag: "🇩🇪", logo: "https://www.thesportsdb.com/images/media/league/badge/0319201680099902.png", category: "Piala/kompetisi" },
@@ -93,13 +78,13 @@ const LEAGUES = [
   { id: 'ger.2.promotion.relegation', name: "German Bundesliga 2 Promotion/Relegation Playoffs", country: "Jerman", flag: "🇩🇪", logo: "https://www.thesportsdb.com/images/media/league/badge/0319201680099902.png", category: "Eropa" },
 
   // Italy
-  { id: 'ita.1', name: "Italian Serie A", country: "Italia", flag: "🇮🇹", logo: "https://www.thesportsdb.com/images/media/league/badge/1932311680100000.png", category: "Eropa", isPopular: true },
+  { id: 'ita.1', name: "Italian Serie A", country: "Italia", flag: "🇮🇹", logo: "https://www.thesportsdb.com/images/media/league/badge/1932311680100000.png", category: "Eropa" },
   { id: 'ita.2', name: "Italian Serie B", country: "Italia", flag: "🇮🇹", logo: "https://www.thesportsdb.com/images/media/league/badge/1932311680100000.png", category: "Eropa" },
   { id: 'ita.coppa_italia', name: "Coppa Italia", country: "Italia", flag: "🇮🇹", logo: "https://www.thesportsdb.com/images/media/league/badge/2522c01534347814.png", category: "Piala/kompetisi" },
   { id: 'ita.super_cup', name: "Italian Supercoppa", country: "Italia", flag: "🇮🇹", logo: "https://www.thesportsdb.com/images/media/league/badge/1932311680100000.png", category: "Piala/kompetisi" },
 
   // France
-  { id: 'fra.1', name: "French Ligue 1", country: "Prancis", flag: "🇫🇷", logo: "https://www.thesportsdb.com/images/media/league/badge/1802311680100052.png", category: "Eropa", isPopular: true },
+  { id: 'fra.1', name: "French Ligue 1", country: "Prancis", flag: "🇫🇷", logo: "https://www.thesportsdb.com/images/media/league/badge/1802311680100052.png", category: "Eropa" },
   { id: 'fra.2', name: "French Ligue 2", country: "Prancis", flag: "🇫🇷", logo: "https://www.thesportsdb.com/images/media/league/badge/1802311680100052.png", category: "Eropa" },
   { id: 'fra.coupe_de_france', name: "Coupe de France", country: "Prancis", flag: "🇫🇷", logo: "https://www.thesportsdb.com/images/media/league/badge/2522c01534347814.png", category: "Piala/kompetisi" },
   { id: 'fra.super_cup', name: "French Trophee des Champions", country: "Prancis", flag: "🇫🇷", logo: "https://www.thesportsdb.com/images/media/league/badge/1802311680100052.png", category: "Piala/kompetisi" },
@@ -197,7 +182,7 @@ const LEAGUES = [
   // Asia / Middle East / Oceania
   { id: 'afc.champions', name: "AFC Champions League Elite", country: "Asia", flag: "🌏", logo: "https://www.thesportsdb.com/images/media/league/badge/5d4x5v1534346808.png", category: "Piala/kompetisi" },
   { id: 'afc.cup', name: "AFC Champions League Two", country: "Asia", flag: "🌏", logo: "https://www.thesportsdb.com/images/media/league/badge/5d4x5v1534346808.png", category: "Piala/kompetisi" },
-  { id: 'afc.asian.cup', name: "AFC Asian Cup", country: "Asia", flag: "🌏", logo: "https://www.thesportsdb.com/images/media/league/badge/5d4x5v1534346808.png", category: "Internasional", isPopular: true },
+  { id: 'afc.asian.cup', name: "AFC Asian Cup", country: "Asia", flag: "🌏", logo: "https://www.thesportsdb.com/images/media/league/badge/5d4x5v1534346808.png", category: "Internasional" },
   { id: 'ksa.1', name: "Saudi Pro League", country: "Arab Saudi", flag: "🇸🇦", logo: "https://upload.wikimedia.org/wikipedia/commons/4/42/Saudi_Pro_League_Logo.svg", category: "Asia" },
   { id: 'ksa.kings.cup', name: "Saudi King's Cup", country: "Arab Saudi", flag: "🇸🇦", logo: "https://upload.wikimedia.org/wikipedia/commons/4/42/Saudi_Pro_League_Logo.svg", category: "Piala/kompetisi" },
   { id: 'jpn.1', name: "Japanese J.League", country: "Jepang", flag: "🇯🇵", logo: "https://upload.wikimedia.org/wikipedia/commons/b/bc/J1_league_logo_2026.svg", category: "Asia" },
@@ -205,7 +190,7 @@ const LEAGUES = [
   { id: 'ind.1', name: "Indian Super League", country: "India", flag: "🇮🇳", logo: "https://www.thesportsdb.com/images/media/league/badge/5d4x5v1534346808.png", category: "Asia" },
   { id: 'tha.1', name: "Thai League 1", country: "Thailand", flag: "🇹🇭", logo: "https://www.thesportsdb.com/images/media/league/badge/5d4x5v1534346808.png", category: "Asia" },
   { id: 'mys.1', name: "Malaysian Super League", country: "Malaysia", flag: "🇲🇾", logo: "https://www.thesportsdb.com/images/media/league/badge/5d4x5v1534346808.png", category: "Asia" },
-  { id: 'idn.1', name: "Liga 1 Indonesia", country: "Indonesia", flag: "🇮🇩", logo: "https://upload.wikimedia.org/wikipedia/id/2/2a/BRI_Super_League_%28Alt%29.svg", category: "Asia", isPopular: true },
+  { id: 'idn.1', name: "Indonesian Super League", country: "Indonesia", flag: "🇮🇩", logo: "https://upload.wikimedia.org/wikipedia/id/2/2a/BRI_Super_League_%28Alt%29.svg", category: "Asia" },
   { id: 'sgp.1', name: "Singaporean Premier League", country: "Singapura", flag: "🇸🇬", logo: "https://www.thesportsdb.com/images/media/league/badge/5d4x5v1534346808.png", category: "Asia" },
   { id: 'aus.1', name: "Australian A-League Men", country: "Australia", flag: "🇦🇺", logo: "https://www.thesportsdb.com/images/media/league/badge/5d4x5v1534346808.png", category: "Asia" },
   { id: 'aus.w.1', name: "Australian A-League Women", country: "Australia", flag: "🇦🇺", logo: "https://www.thesportsdb.com/images/media/league/badge/5d4x5v1534346808.png", category: "Asia" },
